247ffed9820f533696e90250d1d7d0c431d256b0
EverDreenAF <102889302+EverDreenAF@users.noreply.github.com>
2024-11-18 06:09:15 +0000

    v1.1.5 Dashboard Design

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const navigate = useNavigate();
 
   const goToProducts = () => {
-    navigate('/products');
+    navigate('/Dashboard');
   };
 
   return (
--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -2,10 +2,66 @@ import './Dashboard.css';
 
 function Dashboard() {
     return (
-        <div className="headerTitle">
-            TITLE TEST
+        <div className="landingPage">
+    
+          {/* Main Man Header */}
+          <div className="landingPageMainHeader">
+    
+            <div className="mainHeaderNavigation">
+               
+            </div>
+    
+          </div>
+    
+          {/* Anything Header*/}
+          <div className="landingPageHeader">
+    
+            <div className="titleContainer">
+              <div className="title"> {/* Title */}
+                Lorem Ispum
+              </div>
+    
+              <div className="body"> {/* Body */}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Faucibus in libero risus semper habitant arcu eget. 
+              Et integer facilisi eget diam.Lorem ipsum dolor sit ame,consectetur 
+              </div>
+       
+            </div>
+    
+          </div>
+    
+          {/* Anything Body*/}
+          <div className="landingPageBody">
+    
+            <div className="navBox boxOne">
+              <div className="title">
+                Add New Asset
+              </div>
+            </div>
+    
+            <div className="navBox boxTwo">
+              <div className="title">
+                Update Asset
+              </div>
+            </div>
+    
+            <div className="navBox boxThree">
+              <div className="title">
+                Preview Asset
+              </div>
+            </div>
+    
+            <div className="navBox boxThree">
+              <div className="title">
+                Update Asset
+              </div>
+            </div>
+    
+          </div>
+    
         </div>
-    );
+        );
 }
 
 export default Dashboard;
